b4facd66c0d3456924280ae8d8d9a4d2dbacd39b
Lyang Medeiros <lyang.mdrs@gmail.com>
2019-01-10 18:23:17 -0300

    exemplo de GUI com tkinter e openCV

--- vision-sys.py
+++ vision-sys.py
@@ -1,77 +1,66 @@
 ## vision-sys.py 
-# version 0.1
+# version 0.2
 
+import Tkinter 
 import cv2
-import numpy as np
+import PIL.Image, PIL.ImageTk
+import time
 
-# o parametro passado na funcao video capture serve para selecionar a entrada de video.
-# caso esteja desenvolvendo em um pc com apenas uma webcam, deve passar o paramentro '0'
-# em caso de multiplas entradas de video conectadas, o parametro deve mudar de acordo com a entrada desejada.
+class App:
+    def __init__(self, window, window_title, video_source=1):
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = video_source
 
-cap = cv2.VideoCapture(1)
+        self.vid = MyVideoCapture(self.video_source)
+        self.canvas = Tkinter.Canvas(window, width = 1080, height = 720)
+        self.canvas.pack()
 
-# cria a janela onde para os track bars
+        self.btn_snapshot=Tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
+        self.btn_snapshot.pack(anchor=Tkinter.CENTER, expand=True)
 
-cv2.namedWindow('filter')
+        self.delay = 15
+        self.update()
 
-# os track bar, por padrão chamam uma funcao sempre que mudam de valor
-# nesse caso, não queremos nenhuma acao especifica, então chamaremaos a funcao abaixo 
+        self.window.mainloop()
 
-def nothing(x):
-    pass
+    def snapshot(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
 
-cv2.createTrackbar('H_MAX','filter',255,255,nothing)
-cv2.createTrackbar('H_MIN','filter',0,255,nothing)
-cv2.createTrackbar('S_MAX','filter',255,255,nothing)
-cv2.createTrackbar('S_MIN','filter',0,255,nothing)
-cv2.createTrackbar('V_MAX','filter',255,255,nothing)
-cv2.createTrackbar('V_MIN','filter',0,255,nothing)
+        if ret:
+            cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+    def update(self):
+     # Get a frame from the video source
+     ret, frame = self.vid.get_frame()
 
-while(1):
+     if ret:
+         self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+         self.canvas.create_image(0, 0, image = self.photo, anchor = Tkinter.NW)
 
-    # a funcao read() retorna dois valores, ret e frame 
-    # ret é um valor logico que é verdade quando ocorre a captura de um frame corretamente
-    # frame é a matriz com a imagem capturada
+     self.window.after(self.delay, self.update)
 
-    ret, frame = cap.read()
+class MyVideoCapture:
+    def __init__(self, video_source=0):
+        # Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
 
-    # mudando o color space para trabalhar com o padrao HSV que é melhor para detectar borda dos objetos
+        # Get video source width and height
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
-    # pega a posicao dos cursores nas track bars
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
+ 
 
-    H = cv2.getTrackbarPos('H_MAX','filter')
-    S = cv2.getTrackbarPos('S_MAX','filter')
-    V = cv2.getTrackbarPos('V_MAX','filter')
-
-    h = cv2.getTrackbarPos('H_MIN','filter') 
-    s = cv2.getTrackbarPos('S_MIN','filter')
-    v = cv2.getTrackbarPos('V_MIN','filter')
-
-    # representa os valores maximos e minimo hsv em duas variaveis para gerar mascara para o video
-
-    upper = np.array([H,S,V])
-    lower = np.array([h,s,v])
-
-    # gera a mascara 
-
-    mask = cv2.inRange(hsv, lower, upper)
-
-    # faz um and bit a bit entre a mascara e o video original para recortar a cor desejada
-
-    res = cv2.bitwise_and(frame,frame, mask= mask)
-
-    # mostra os frames em janelas separadas
-
-    cv2.imshow('BRG Image', frame)
-    cv2.imshow('HSV Image', hsv)
-    cv2.imshow('Result', res)
-    
-    # pressione 'Esc' para sair
-
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
-
-cv2.destroyAllWindows()
+App(Tkinter.Tk(),"Tkinter and OpenCV")
