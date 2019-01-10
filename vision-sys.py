a064275a78cbc92ade607e3dee9d36d7fc9507a4
Lyang Medeiros <lyang.mdrs@gmail.com>
2019-01-09 23:19:52 -0300

    janela com trckbars para selecao de cores em tempo real

--- vision-sys.py
+++ vision-sys.py
@@ -1,9 +1,21 @@
 ## vision-sys.py 
-  #teste git
+# version 0.1
+
 import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(1)
+cv2.namedWindow('filter')
+
+def nothing(x):
+    pass
+
+cv2.createTrackbar('H_MAX','filter',0,255,nothing)
+cv2.createTrackbar('H_MIN','filter',0,255,nothing)
+cv2.createTrackbar('S_MAX','filter',0,255,nothing)
+cv2.createTrackbar('S_MIN','filter',0,255,nothing)
+cv2.createTrackbar('V_MAX','filter',0,255,nothing)
+cv2.createTrackbar('V_MIN','filter',0,255,nothing)
 
 while(1):
 
@@ -12,20 +24,26 @@ while(1):
 
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    H = cv2.getTrackbarPos('H_MAX','filter')
+    S = cv2.getTrackbarPos('S_MAX','filter')
+    V = cv2.getTrackbarPos('V_MAX','filter')
+    h = cv2.getTrackbarPos('H_MIN','filter') 
+    s = cv2.getTrackbarPos('S_MIN','filter')
+    v = cv2.getTrackbarPos('V_MIN','filter')
 
-    # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    upper = np.array([H,S,V])
+    lower = np.array([h,s,v])
 
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    mask = cv2.inRange(hsv, lower, upper)
 
-    # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
 
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
+
+    cv2.imshow('BRG Image', frame)
+    cv2.imshow('HSV Image', hsv)
+    cv2.imshow('Result', res)
+    
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
