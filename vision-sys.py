ad17feadf239829857688e84ae12f256e8d2be54
Lyang Medeiros <lyang.mdrs@gmail.com>
2019-01-11 21:38:25 -0300

    gui terminada mais ainda não funcional

--- vision-sys.py
+++ vision-sys.py
@@ -22,52 +22,61 @@ class App:
         self.video_source = video_source
 
         self.resize_factor = 0.5
+        self.name = StringVar()
 
-        self.window.geometry('650x300+600+200')
+        #self.window.geometry('650x400+600+200')
         self.vid = MyVideoCapture(self.video_source)
 
-        self.canvas = Canvas(window, width = self.vid.width*(self.resize_factor) , height = self.vid.height*(self.resize_factor))
-        self.canvas.grid(row=0,column=0)
+        self.label_RGB = Label(window, text = 'Real Time Tracking').grid(row=0,column=0)
+        self.label_Mask = Label(window, text = 'HSV Maks Veiwer').grid(row=0,column=1)
 
-        self.canvas2 = Canvas(window, width = self.vid.width*(self.resize_factor) , height = self.vid.height*(self.resize_factor))
-        self.canvas2.grid(row=0,column=1)
+        self.canvas_rgb = Canvas(window, width = self.vid.width*(self.resize_factor) , height = self.vid.height*(self.resize_factor))
+        self.canvas_rgb.grid(row=1,column=0)
+
+        self.canvas_hsv = Canvas(window, width = self.vid.width*(self.resize_factor) , height = self.vid.height*(self.resize_factor))
+        self.canvas_hsv.grid(row=1,column=1)
 
         self.delay = 15
         self.update()
 
         # H value sliders
-        '''        self.HMax_label = Label(text = 'HMax').place(x=xpos,y=ypos) 
-        self.slider = Scale(self.window, orient=HORIZONTAL, length=300, from_=0, to=255).place(x=xpos+xspace,y=ypos+yspace)
-        self.HMax_label = Label(text = 'HMin').place(x=xpos,y=ypos+degree) 
-        self.slider = Scale(self.window, orient=HORIZONTAL, length=300, from_=0, to=255).place(x=xpos+xspace,y=ypos+yspace+degree)
+        self.slider_hmax = Scale(self.window, orient=HORIZONTAL, label = 'HMax', length=300, from_=0, to=255).grid(row=2, column=0)
+        self.slider_hmin = Scale(self.window, orient=HORIZONTAL, label = 'HMin', length=300, from_=0, to=255).grid(row=2, column=1)
 
         # S value sliders
-        self.HMax_label = Label(text = 'SMax').place(x=xpos,y=ypos+degree*2) 
-        self.slider = Scale(self.window, orient=HORIZONTAL, length=300, from_=0, to=255).place(x=xpos+xspace,y=ypos+yspace+degree*2)
-        self.HMax_label = Label(text = 'SMin').place(x=xpos,y=ypos+degree*3) 
-        self.slider = Scale(self.window, orient=HORIZONTAL, length=300, from_=0, to=255).place(x=xpos+xspace,y=ypos+yspace+degree*3)
+        self.slider_smax = Scale(self.window, orient=HORIZONTAL, label = 'SMax', length=300, from_=0, to=255).grid(row=3, column=0)
+        self.slider_smin = Scale(self.window, orient=HORIZONTAL, label = 'SMin', length=300, from_=0, to=255).grid(row=3, column=1)
 
         # V value sliders
-        self.HMax_label = Label(text = 'VMax').place(x=xpos,y=ypos+degree*4) 
-        self.slider = Scale(self.window, orient=HORIZONTAL, length=300, from_=0, to=255).place(x=xpos+xspace,y=ypos+degree*4-10)
-        self.HMax_label = Label(text = 'VMin').place(x=xpos,y=ypos+degree*5) 
-        self.slider = Scale(self.window, orient=HORIZONTAL, length=300, from_=0, to=255).place(x=xpos+xspace,y=ypos+yspace+degree*5)'''
+        self.slider_vmax = Scale(self.window, orient=HORIZONTAL, label = 'VMax', length=300, from_=0, to=255).grid(row=4, column=0)
+        self.slider_vmin = Scale(self.window, orient=HORIZONTAL, label = 'VMin', length=300, from_=0, to=255).grid(row=4, column=1)
 
+        self.name_text_box = Entry(window, textvariable=self.name).grid(row=5, column=0)
 
+        self.btn_save = Button(window, text='Save', command=self.save).grid(row=6, column=0)
+        self.btn_rst = Button(window, text='Reset Mask Values', command=self.rst).grid(row=5,column=1) 
+        
         self.window.mainloop()
+
+    def save(self):
+        pass
+
+    def rst(self):
+        pass
 
     def update(self):
         # Get a frame from the video source
         ret, frame = self.vid.get_frame()
         frame = self.vid.rescale_frame(ret,frame,self.resize_factor)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
         if ret:
-            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
-            self.canvas.create_image(0, 0, image = self.photo, anchor = NW)
-            self.photo2 = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(hsv))
-            self.canvas2.create_image(0, 0, image = self.photo2, anchor = NW)
+            self.photo_rgb = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(rgb))
+            self.canvas_rgb.create_image(0, 0, image = self.photo_rgb, anchor = NW)
+            self.photo_hsv = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(hsv))
+            self.canvas_hsv.create_image(0, 0, image = self.photo_hsv, anchor = NW)
 
         self.window.after(self.delay, self.update)
 
