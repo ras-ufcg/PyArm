1e23037221d5109397466600c6a56567c6f31b67
Lyang Medeiros <lyang.mdrs@gmail.com>
2019-01-12 12:52:53 -0300

    fomação de máscaras funcionando

--- vision-sys.py
+++ vision-sys.py
@@ -5,6 +5,7 @@ from Tkinter import *
 import cv2
 import PIL.Image, PIL.ImageTk
 import time
+import numpy as np
 
 class App:
     def __init__(self, window, window_title, video_source):
@@ -13,10 +14,17 @@ class App:
         self.window.title(window_title)
         self.video_source = video_source
 
+        self.hmax = DoubleVar()
+        self.hmin = DoubleVar()
+        self.smax = DoubleVar()
+        self.smin = DoubleVar()
+        self.vmax = DoubleVar()
+        self.vmin = DoubleVar()
+
         self.resize_factor = 0.5
         self.name = StringVar()
 
-        #self.window.geometry('650x400+600+200')
+        self.window.geometry('650x500+600+200')
         self.vid = MyVideoCapture(self.video_source)
 
         self.label_RGB = Label(window, text = 'Real Time Tracking').grid(row=0,column=0)
@@ -32,17 +40,22 @@ class App:
         self.update()
 
         # H value sliders
-        self.slider_hmax = Scale(self.window, orient=HORIZONTAL, label = 'HMax', length=300, from_=0, to=255).grid(row=2, column=0)
-        self.slider_hmin = Scale(self.window, orient=HORIZONTAL, label = 'HMin', length=300, from_=0, to=255).grid(row=2, column=1)
-
+        self.slider_hmax = Scale(self.window, orient=HORIZONTAL, variable = self.hmax , label = 'HMax', length=300, from_=0, to=255)
+        self.slider_hmax.grid(row=2, column=1)
+        self.slider_hmax.set(255)
+        slider_hmin = Scale(self.window, orient=HORIZONTAL, variable = self.hmin , label = 'HMin', length=300, from_=0, to=255).grid(row=2, column=0)
         # S value sliders
-        self.slider_smax = Scale(self.window, orient=HORIZONTAL, label = 'SMax', length=300, from_=0, to=255).grid(row=3, column=0)
-        self.slider_smin = Scale(self.window, orient=HORIZONTAL, label = 'SMin', length=300, from_=0, to=255).grid(row=3, column=1)
-
+        self.slider_smax = Scale(self.window, orient=HORIZONTAL, variable = self.smax , label = 'SMax', length=300, from_=0, to=255)
+        self.slider_smax.grid(row=3, column=1)
+        self.slider_smax.set(255)
+        self.slider_smin = Scale(self.window, orient=HORIZONTAL, variable = self.smin , label = 'SMin', length=300, from_=0, to=255).grid(row=3, column=0)
+        
         # V value sliders
-        self.slider_vmax = Scale(self.window, orient=HORIZONTAL, label = 'VMax', length=300, from_=0, to=255).grid(row=4, column=0)
-        self.slider_vmin = Scale(self.window, orient=HORIZONTAL, label = 'VMin', length=300, from_=0, to=255).grid(row=4, column=1)
-
+        self.slider_vmax = Scale(self.window, orient=HORIZONTAL, variable = self.vmax, label = 'VMax', length=300, from_=0, to=255)
+        self.slider_vmax.grid(row=4, column=1)
+        self.slider_vmax.set(255)
+        self.slider_vmin = Scale(self.window, orient=HORIZONTAL, variable = self.vmin, label = 'VMin', length=300, from_=0, to=255).grid(row=4, column=0)
+        
         self.name_text_box = Entry(window, textvariable=self.name).grid(row=5, column=0)
 
         self.btn_save = Button(window, text='Save', command=self.save).grid(row=6, column=0)
@@ -64,10 +77,17 @@ class App:
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+        upper = np.array([self.hmax.get(), self.smax.get(), self.vmax.get()])
+        lower = np.array([self.hmin.get(), self.smin.get(), self.vmin.get()])
+
+        mask = cv2.inRange(hsv, lower, upper)
+
+        res = cv2.bitwise_and(frame, frame, mask = mask)
+
         if ret:
             self.photo_rgb = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(rgb))
             self.canvas_rgb.create_image(0, 0, image = self.photo_rgb, anchor = NW)
-            self.photo_hsv = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(hsv))
+            self.photo_hsv = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(res))
             self.canvas_hsv.create_image(0, 0, image = self.photo_hsv, anchor = NW)
 
         self.window.after(self.delay, self.update)
