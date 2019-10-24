53f872292798fc018d68c4e4c41ea21cd5c15a9f
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2019-10-24 02:20:50 -0300

    espera pelo comando pra todos os motores

--- controlador/controlador.ino
+++ controlador/controlador.ino
@@ -8,16 +8,16 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define DEBUG     true
-#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+#define DEBUG       true
+#define SERVOMIN    150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX    600 // this is the 'maximum' pulse length count (out of 4096)
+#define DOF         5
 
 uint8_t servonum = 0;
 char terminator = '\n';
 int comprimento = 3, // o correto aqui é 3, mas para os testes com o serialMonitor tem que ser 4 por causa do enter.
     contadorDeComandos = 0;
 int comandoCompleto[5];
-//char entrada[comprimento] = {'\0', '\0', '\0', '\0'};
 
 
 void setup() {
@@ -35,9 +35,22 @@ void loop() {
   if(Serial.available())
   {
     String entrada = Serial.readStringUntil(terminator);
-    Serial.print("Recebi: ");
     int angulo = entrada.toInt();
+    
+    Serial.print("Recebi: ");
     Serial.println(angulo);
+    
+    Serial.print("Número de comandos recebidos até agora: ");
+    Serial.println(contadorDeComandos);
+
+    comandoCompleto[contadorDeComandos++] = angulo;
+
+    if(contadorDeComandos > DOF)
+    {
+        contadorDeComandos = 0;
+        //executaComandos();
+    }
+
     int pulselength = map(angulo, 0, 180, SERVOMIN, SERVOMAX);
     pwm.setPWM(15, 0, pulselength);
   }
