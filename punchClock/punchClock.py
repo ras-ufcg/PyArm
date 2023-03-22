30ac960fc36cf5ebab4e3a34277d4aa63a3f8ceb
Breno Meneses <50165797+brenopmeneses@users.noreply.github.com>
2023-03-21 21:43:54 -0300

    Add code for punchClock in Python

---
+++ punchClock/punchClock.py
@@ -0,0 +1,43 @@
+# please run "pip install gitpython"
+# run this only from root folder
+# make sure git user is configured
+
+from datetime import datetime
+import os
+from git import Repo
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description=''' Punch clock for R and D projects. ''')
+parser.add_argument('name', help='Your name')
+parser.add_argument('mode', help='"in" (for starting counter) or "out" (to push end commit)')
+args = parser.parse_args()
+
+name = args.name
+mode = args.mode
+
+now = datetime.now()
+myfolder = os.path.abspath(os.getcwd())
+
+current_time = now.strftime("%H:%M:%S")
+current_day = datetime.today().strftime('%Y-%m-%d')
+
+if mode == 'out':
+    task = input("Enter what you've done: ")
+    text = current_time+" >> "+task+"\n"
+    msg = "Punch created for "+name+" at "+current_day+" - "+current_time
+else:
+    text = current_day+" : "+current_time+" - "
+    msg = "In registred for "+name+" at "+current_day+" - "+current_time
+
+f = open('./'+name+'.txt','a+')
+f.write(text)
+f.close()
+print(msg)
+
+PATH_OF_GIT_REPO = myfolder+'/.git'  # make sure .git folder is properly configured
+repo = Repo(PATH_OF_GIT_REPO)
+repo.git.add('./'+name+'.txt')
+repo.index.commit(msg)
+origin = repo.remote(name='braco')
+origin.push()
